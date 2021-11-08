1655040a73245f89df1e544660c91be14995bc4e
Jury <jurypassa@gmail.com>
2021-11-08 15:54:44 +0100

    addEventListener

---
+++ main.js
@@ -0,0 +1,9 @@
+const hidden = document.querySelector("i.close");
+
+hidden.addEventListener("click", function(){
+    alert("ciao");
+
+    // const hamburger = document.querySelector(".hamburger-menu");
+
+    // hamburger.className += ".active";
+});
